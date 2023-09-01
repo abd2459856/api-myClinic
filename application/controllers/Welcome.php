ccb1eaffd51f3bd2634eb409a79451381d7cf4dc
wanatthaphong.p <wanatthaphong.p>
2023-09-01 17:31:57 +0700

    01/09/2566

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Welcome extends CI_Controller {
+defined('BASEPATH') or exit('No direct script access allowed');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -16,18 +21,197 @@ class Welcome extends CI_Controller {
 	 *
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 * @see https://codeigniter.com/userguide3/general/u$this->info['Type_Realty'];rls.html
 	 */
+	private $info = "";
 	public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Get_model');
-    }
+	{
+		parent::__construct();
+		$this->load->model('Get_model');
+		$this->info = $this->input->post();
+	}
 
 	public function index()
 	{
-		$doctor =	$this->Get_model->get_doctor();
-		print_r($doctor);
-		$this->load->view('welcome_message');
+	}
+	public function get_doctor()
+	{
+		return $this->Get_model->get_doctor();
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+
+	public function insert_doctor()
+	{
+		$data = [
+			"Fisrtname" => $this->info['Fisrtname'],
+			"Lastname" => $this->info['Lastname'],
+		];
+		$this->Get_model->insert_doctor($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+
+	public function update_doctor()
+	{
+		$data = [
+			"Fisrtname" => $this->info['Fisrtname'],
+			"Lastname" => $this->info['Lastname'],
+			"ID_doctor" => $this->info['ID_doctor'],
+		];
+		$this->Get_model->insert_doctor($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+
+	public function insert_rendezvous()
+	{
+
+		$data = [
+			"Savedate" => $this->info['Savedate'],
+			"Startdate" => $this->info['Startdate'],
+			"StartTime" => $this->info['StartTime'],
+			"Doctor_ID" => $this->info['Doctor_ID'],
+			"Room_ID" => $this->info['Room_ID'],
+			"Section" => $this->info['Section'],
+			"Remark" => $this->info['Remark'],
+			"Phone" => $this->info['Phone'],
+			"Patient_ID" => $this->info['Patient_ID'],
+		];
+		$this->Get_model->insert_rendezvous($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function update_rendezvous()
+	{
+		$data = [
+			"id" => $this->info['id'],
+			"status" => $this->info['status'],
+			"Savedate" => $this->info['Savedate'],
+		];
+		$this->Get_model->update_rendezvous($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+
+	public function insert_customer()
+	{
+		$data = [
+			"IDCard" => $this->info['IDCard'],
+			"Nickname" => $this->info['Nickname'],
+			"Prefix" => $this->info['Prefix'],
+			"Fisrtname" => $this->info['Fisrtname'],
+			"Lastname" => $this->info['Lastname'],
+			"Birthday" => $this->info['Birthday'],
+			"Occupation" => $this->info['Occupation'],
+			"Race" => $this->info['Race'],
+			"Nationality" => $this->info['Nationality'],
+			"religion" => $this->info['religion'],
+			"status_relationship" => $this->info['status_relationship'],
+			"weight" => $this->info['weight'],
+			"height" => $this->info['height'],
+			"address_number" => $this->info['address_number'],
+			"address_moo" => $this->info['address_moo'],
+			"address_village" => $this->info['address_village'],
+			"address_soi" => $this->info['address_soi'],
+			"address_road" => $this->info['address_road'],
+			"address_subdistrict" => $this->info['address_subdistrict'],
+			"address_district" => $this->info['address_district'],
+			"address_province" => $this->info['address_province'],
+			"postal" => $this->info['postal'],
+			"tell" => $this->info['tell'],
+			"email" => $this->info['email'],
+			"profile" => $this->info['profile'],
+		];
+		$this->Get_model->insert_customer($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function Update_customer()
+	{
+		$data = [
+			"Customer_ID" => $this->info['Customer_ID'],
+			"IDCard" => $this->info['IDCard'],
+			"Nickname" => $this->info['Nickname'],
+			"Prefix" => $this->info['Prefix'],
+			"Fisrtname" => $this->info['Fisrtname'],
+			"Lastname" => $this->info['Lastname'],
+			"Birthday" => $this->info['Birthday'],
+			"Occupation" => $this->info['Occupation'],
+			"Race" => $this->info['Race'],
+			"Nationality" => $this->info['Nationality'],
+			"religion" => $this->info['religion'],
+			"status_relationship" => $this->info['status_relationship'],
+			"weight" => $this->info['weight'],
+			"height" => $this->info['height'],
+			"address_number" => $this->info['address_number'],
+			"address_moo" => $this->info['address_moo'],
+			"address_village" => $this->info['address_village'],
+			"address_soi" => $this->info['address_soi'],
+			"address_road" => $this->info['address_road'],
+			"address_subdistrict" => $this->info['address_subdistrict'],
+			"address_district" => $this->info['address_district'],
+			"address_province" => $this->info['address_province'],
+			"postal" => $this->info['postal'],
+			"tell" => $this->info['tell'],
+			"email" => $this->info['email'],
+			"profile" => $this->info['profile'],
+		];
+		$this->Get_model->Update_customer($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function get_customer()
+	{
+		return $this->Get_model->get_customer();
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function img()
+	{
+		echo count($_FILES['images']['name']);
+		
+		// echo $name  = $_FILES['images']['name'];
+		
+		// move_uploaded_file($_FILES['images']['tmp_name'], 'Img/' . $name);
+	}
+	public function get_type()
+	{
+		return $this->Get_model->get_type();
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function insert_type()
+	{
+		$data = [
+			"name" => $this->info['name']
+		];
+		$this->Get_model->insert_type($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function update_type()
+	{
+		$data = [
+			"id" => $this->info['id'],
+			"name" => $this->info['name']
+		];
+		$this->Get_model->update_type($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
+	}
+	public function insert_img()
+	{
+		$data = [
+			"name" => $this->info['name'],
+			"id_type" => $this->info['id_type'],
+			"id_rendezvous" => $this->info['id_rendezvous'],
+			"id_customer" => $this->info['id_customer'],
+			"filepath" => $this->info['filepath'],
+			"extension" => $this->info['extension'],
+		];
+		$this->Get_model->insert_img($data);
+		http_response_code(200);
+		echo json_encode(array('result' => true));
 	}
 }
--- application/models/Get_model.php
+++ application/models/Get_model.php
@@ -13,4 +13,217 @@ class Get_model extends CI_Model
         $sql = "SELECT * FROM `tbl_doctor`";
         return $this->db->query($sql)->result();
     }
+
+    public function insert_doctor($data)
+    {
+        $sql = "INSERT INTO tbl_doctor
+                (
+                    Fisrtname
+                    Lastname
+                )
+                VALUES
+                (
+                    '$data[Fisrtname]',
+                    '$data[Lastname]'
+                )";
+        return $this->db->query($sql);
+    }
+
+    public function update_doctor($data)
+    {
+        $sql = "UPDATE tbl_doctor SET 
+                Fisrtname = '$data[Fisrtname]',
+                Lastname = '$data[Lastname]'
+                WHERE ID_doctor = '$data[ID_doctor]'";
+        return $this->db->query($sql);
+    }
+
+    public function get_rendezvous($data)
+    {
+        $WHERE = "";
+        if ($data['id'] != '') {
+            $WHERE = "AND id = '$data[id]' ";
+        }
+        $sql = "SELECT * FROM Tbl_rendezvous WHERE 1=1 " . $WHERE;
+        return $this->db->query($sql)->result();
+    }
+
+    public function insert_rendezvous($data)
+    {
+        $sql = "INSERT INTO Tbl_rendezvous
+                (
+                    Savedate,
+                    Startdate,
+                    StartTime,
+                    Doctor_ID,
+                    Room_ID,
+                    Section,
+                    Remark,
+                    Phone,
+                    Patient_ID.
+                )
+                VALUES
+                (
+                    '$data[Savedate]',
+                    '$data[Startdate]',
+                    '$data[StartTime]',
+                    '$data[Doctor_ID]',
+                    '$data[Room_ID]',
+                    '$data[Section]',
+                    '$data[Remark]',
+                    '$data[Phone]',
+                    '$data[Patient_ID]'
+                )";
+        return $this->db->query($sql);
+    }
+    public function update_rendezvous($data)
+    {
+        $sql = "UPDATE Tbl_rendezvous 
+                SET status = '$data[status]',
+                    Savedate = '$data[Savedate]'
+                WHERE 	id = '$data[id]' ";
+    }
+    public function insert_customer($data)
+    {
+        $sql = "INSERT INTO Tbl_customer
+        (
+            IDCard,
+            Nickname,
+            Prefix,
+            Fisrtname,
+            Lastname,
+            Birthday,
+            Occupation,
+            Race,
+            Nationality,
+            religion,
+            status_relationship,
+            weight,
+            height,
+            address_number,
+            address_moo,
+            address_village,
+            address_soi,
+            address_road,
+            address_subdistrict,
+            address_district,
+            address_province,
+            postal,
+            tell,
+            email,
+            profile
+        )
+        VALUES
+        (
+            '$data[IDCard]',
+            '$data[Nickname]',
+            '$data[Prefix]',
+            '$data[Fisrtname]',
+            '$data[Lastname]',
+            '$data[Birthday]',
+            '$data[Occupation]',
+            '$data[Race]',
+            '$data[Nationality]',
+            '$data[religion]',
+            '$data[status_relationship]',
+            '$data[weight]',
+            '$data[height]',
+            '$data[address_number]',
+            '$data[address_moo]',
+            '$data[address_village]',
+            '$data[address_soi]',
+            '$data[address_road]',
+            '$data[address_subdistrict]',
+            '$data[address_district]',
+            '$data[address_province]',
+            '$data[postal]',
+            '$data[tell]',
+            '$data[email]',
+            '$data[profile]'
+        )";
+        return $this->db->query($sql);
+    }
+    public function Update_customer($data)
+    {
+        $sql = "UPDATE Tbl_customer SET 
+            Nickname = '$data[Nickname]',
+            Prefix = '$data[Prefix]',
+            Fisrtname = '$data[Fisrtname]',
+            Lastname = '$data[Lastname]',
+            Birthday = '$data[Birthday]',
+            Occupation = '$data[Occupation]',
+            Race = '$data[Race]',
+            Nationality = '$data[Nationality]',
+            religion = '$data[religion]',
+            status_relationship = '$data[status_relationship]',
+            weight = '$data[weight]',
+            height = '$data[height]',
+            address_number = '$data[address_number]',
+            address_moo = '$data[address_moo]',
+            address_village = '$data[address_village]',
+            address_soi = '$data[address_soi]',
+            address_road = '$data[address_road]',
+            address_subdistrict = '$data[address_subdistrict]',
+            address_district = '$data[address_district]',
+            address_province = '$data[address_province]',
+            postal = '$data[postal]',
+            tell = '$data[tell]',
+            email= '$data[email]',
+            profile = '$data[profile]'
+                WHERE Customer_ID = '$data[Customer_ID]'";
+        return $this->db->query($sql);
+    }
+    public function get_customer($data)
+    {
+        $WHERE = "";
+        if ($data['Customer_ID'] != '') {
+            $WHERE = "AND Customer_ID = '$data[Customer_ID]' ";
+        }
+        $sql = "SELETE * FROM Tbl_customer WHERE 1=1" . $WHERE;
+        return $this->db->query($sql)->result();
+    }
+    public function get_type()
+    {
+        $sql = "SELETE * FROM tbl_type";
+        return $this->db->query($sql)->result();
+    }
+    public function insert_type($data)
+    {
+        $sql = "INSERT INTO tbl_type
+                (
+                    name
+                )
+                VALUES
+                (
+                    '$data[name]'
+                )";
+        return $this->db->query($sql);
+    }
+    public function update_type($data)
+    {
+        $sql = "UPDATE tbl_type set name = '$data[name]' where id = '$data [id]' ";
+        return $this->db->query($sql);
+    }
+    public function insert_img($data)
+    {
+        $sql = "INSERT INTO tbl_image
+                (
+                    name,
+                    id_type,
+                    id_rendezvous,
+                    id_customer,
+                    filepath,
+                    extension
+                )
+                VALUES
+                (
+                    '$data[name]',
+                    '$data[id_type]',
+                    '$data[id_rendezvous]',
+                    '$data[id_customer]',
+                    '$data[filepath]',
+                    '$data[extension]'
+                )";
+        return $this->db->query($sql);
+    }
 }
