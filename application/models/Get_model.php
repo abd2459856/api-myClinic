deed78e15a5fabcd2e3a3ea20fa147ab63357542
wanatthaphong.p <wanatthaphong.p>
2023-09-04 12:33:56 +0700

    -

--- application/models/Get_model.php
+++ application/models/Get_model.php
@@ -210,7 +210,7 @@ class Get_model extends CI_Model
         FROM tbl_package_treat P 
         INNER JOIN tbl_appointment A ON A.ID_package = p.ID_treat 
         LEFT JOIN tbl_treatments T ON A.ID_package = T.ID_pagekage_treat 
-        WHERE A.ID_customer = '1'  $Where";
+        WHERE A.ID_customer = '$data[IDCus]'  $Where";
         // $sql = "SELECT p.ID_treat,p.treat_name,T.treatmens_detail,T.Date_save,A.ID_nut,A.ID_package,A.ID_customer
         // FROM tbl_package_treat P
         // INNER JOIN tbl_appointment A ON A.ID_package =p.ID_treat
