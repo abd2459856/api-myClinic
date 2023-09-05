54c162c98bb3b77f97bb35aae381efee172569fd
wanatthaphong.p <wanatthaphong.p>
2023-09-05 23:25:34 +0700

    05/09/2566

--- application/models/Get_model.php
+++ application/models/Get_model.php
@@ -216,7 +216,7 @@ class Get_model extends CI_Model
             $Where = "AND T.ID_pagekage_treat = '$data[ID_treat]'";
         }
         
-        $sql="SELECT p.ID_treat,p.treat_name,A.ID_nut,A.ID_package,A.ID_customer,T.ID_treatments
+        $sql="SELECT p.ID_treat,p.treat_name,A.ID_nut,A.ID_package,A.ID_customer,T.ID_treatments,T.treatmens_detail,T.Date_save
         FROM tbl_package_treat P 
         INNER JOIN tbl_appointment A ON A.ID_package = p.ID_treat 
         LEFT JOIN tbl_treatments T ON A.ID_package = T.ID_pagekage_treat 
